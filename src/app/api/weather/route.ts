9357965662637047dc1ffabd72d3c59bc2843f67
qwc-ch <zzzzzzxx2022@163.com>
2026-06-18 19:01:19 +0800

    修改天气api

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -10,44 +10,44 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const revalidate = 0;
 
-// const AMAP_KEY = process.env.AMAP_KEY;
+const AMAP_KEY = process.env.AMAP_KEY;
 
 export const GET = async (req: NextRequest) => {
   const ip = req.headers.get("x-real-ip") || req.headers.get("x-forwarded-for");
   const clientIp = ip != "::1" && ip != null ? ip : undefined;
-  // if (!AMAP_KEY) {
-  //   const res: any = await onOio("weather", {}, req.headers);
-  //   if (res?.code == 200 && res?.result) {
-  //     const {
-  //       result: { city, condition },
-  //     } = res;
-  //     return NextResponse.json({
-  //       data: {
-  //         apiKey: "oio",
-  //         province: city?.Province,
-  //         city: city?.City,
-  //         carrier: city?.Carrier,
-  //         adcode: null,
-  //         weather: condition?.day_weather,
-  //         temperature: condition?.max_degree,
-  //         winddirection: condition?.day_wind_direction,
-  //         windpower: condition?.day_wind_power,
-  //         humidity: condition?.aqi?.aqi,
-  //         reporttime: condition?.aqi?.update_time,
-  //         temperature_float: null,
-  //         humidity_float: null,
-  //       },
-  //       success: true,
-  //       message: "success",
-  //     });
-  //   } else {
-  //     return NextResponse.json({
-  //       data: "error: oio",
-  //       success: false,
-  //       message: "fail",
-  //     });
-  //   }
-  // }
+  if (!AMAP_KEY) {
+    const res: any = await onOio("weather", {}, req.headers);
+    if (res?.code == 200 && res?.result) {
+      const {
+        result: { city, condition },
+      } = res;
+      return NextResponse.json({
+        data: {
+          apiKey: "oio",
+          province: city?.Province,
+          city: city?.City,
+          carrier: city?.Carrier,
+          adcode: null,
+          weather: condition?.day_weather,
+          temperature: condition?.max_degree,
+          winddirection: condition?.day_wind_direction,
+          windpower: condition?.day_wind_power,
+          humidity: condition?.aqi?.aqi,
+          reporttime: condition?.aqi?.update_time,
+          temperature_float: null,
+          humidity_float: null,
+        },
+        success: true,
+        message: "success",
+      });
+    } else {
+      return NextResponse.json({
+        data: "error: oio",
+        success: false,
+        message: "fail",
+      });
+    }
+  }
 
   const resIp: any =
     (await onAmap(
